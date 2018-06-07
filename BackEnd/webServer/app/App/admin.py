55ebcf7f42f4cd38cb8b4c52d27672bea42aace1
Yicong M. Gong <gongyc3@gmail.com>
2018-06-07 14:33:24 -0700

    Backend updated story writer and story reader

--- BackEnd/webServer/app/App/admin.py
+++ BackEnd/webServer/app/App/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import City, Story
 # Register your models here.
+admin.site.register(City)
+admin.site.register(Story)
--- BackEnd/webServer/app/App/models.py
+++ BackEnd/webServer/app/App/models.py
@@ -1,11 +1,40 @@
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, User
 from django.db import models
 from django.contrib.auth.models import BaseUserManager
+# time zone
+from django.utils import timezone
+# uuid
+import uuid
+
+"""
+Potential User Profile
+"""
+# class Profile(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     bio = models.TextField(max_length=500, blank=True)
+#     location = models.CharField(max_length=30, blank=True)
+#     birth_date = models.DateField(null=True, blank=True)
+
+# @receiver(post_save, sender=User)
+# def create_user_profile(sender, instance, created, **kwargs):
+#     if created:
+#         Profile.objects.create(user=instance)
+
+# @receiver(post_save, sender=User)
+# def save_user_profile(sender, instance, **kwargs):
+#     instance.profile.save()
 
 class City(models.Model):
-    cityName = models.CharField(max_length = 30)
-    stateName = models.CharField(max_length = 30)
-    countryName = models.CharField(max_length = 30)
+    city = models.CharField(max_length = 50, primary_key = True, unique = True)
+    city_name = models.CharField(max_length = 20)
+    state_name = models.CharField(max_length = 20)
+    country_name = models.CharField(max_length = 20)
 
 class Story(models.Model):
+    id = models.CharField(max_length = 50, primary_key = True, unique = True)
     city = models.ForeignKey(City, on_delete=models.CASCADE)
+    title = models.CharField(max_length = 50)
+    summary = models.CharField(max_length = 150)
+    content = models.CharField(max_length = 9000)
+    like = models.IntegerField(default=0)
+    date = models.DateField(default=timezone.now)
