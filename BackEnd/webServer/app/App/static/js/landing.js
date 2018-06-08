83ad31022f9136db9e0bdda4321abf60df446021
Yicong M. Gong <gongyc3@gmail.com>
2018-06-08 00:50:16 -0700

    Updated landing page to use GeoByte API instead of GMap API

--- BackEnd/webServer/app/App/static/js/landing.js
+++ BackEnd/webServer/app/App/static/js/landing.js
@@ -3,3 +3,24 @@ function showCity(){
     var url = 'http://0.0.0.0:8000/app/city?city_name='+city;
     window.location.replace(url);
 }
+
+function validateCity(){
+    // Validate city name
+    var cityName = document.getElementById('city').value;
+    if (!cityName) {
+        alert('Please enter city name');
+        return false;
+    }
+    cityfqcn = cityName;
+    if (cityfqcn) {
+        jQuery.getJSON(
+                "http://gd.geobytes.com/GetCityDetails?callback=?&fqcn="+cityfqcn,
+                function (data) {
+                    if (data.geobytescityid>0) {
+                        showCity();
+                    }
+                    else alert('Please select city from drop down menu')
+                }
+            );
+    }
+}
